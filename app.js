0c51356ef7bf83543809fa537ac8cec9ad08d228
bkimz <writetobriankim@gmail.com>
2017-06-20 16:41:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+ var friends = ["Tony", "Eliza", "Pair", "Markus", "Lily"];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/friends", function(req, res){
+   
+   res.render("friends", {friends: friends}); 
+});
+
+app.post("/addfriend", function(req, res){
+    console.log(req.body);
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    
+   res.redirect("/friends"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server has started");
+});
